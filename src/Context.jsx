d8d11de56334c528bdb0269565c8030e3014c461
karolina.kukowska@gmail.com <64924406+karola-kasprzak@users.noreply.github.com>
2023-06-19 13:11:13 +0200

    api integration

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,19 +1,31 @@
 import { createContext, useContext, useState, useEffect } from "react";
 
 const GlobalContext = createContext();
-
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const url = `https://api.unsplash.com/search/photos/?client_id=${
+    import.meta.env.VITE_API_KEY
+  }&page=1`;
+  const getThemePrefference = () => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-schema:dark)"
+    ).matches;
+    const storedDarkMode = localStorage.getItem("darkTheme") === "true";
+    return storedDarkMode || prefersDark;
+  };
+  const [isDarkTheme, setIsDarkTheme] = useState(getThemePrefference());
+  const [searchTerm, setSearchTerm] = useState("dog");
   const toggleDarkTheme = () => {
     const newTheme = !isDarkTheme;
     setIsDarkTheme(newTheme);
-    const body = document.querySelector("body");
-    body.classList.toggle("dark-theme", newTheme);
-    //console.log(body);
+    localStorage.setItem("darkTheme", newTheme);
   };
-
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
   return (
-    <GlobalContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+    <GlobalContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, url, searchTerm, setSearchTerm }}
+    >
       {children}
     </GlobalContext.Provider>
   );
--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useGlobalContext } from "./Context";
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const { setSearchTerm } = useGlobalContext();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const searchValue = event.target.elements.search.value;
+    if (!searchValue) return;
+    setSearchTerm(searchValue);
+  };
+
+  return (
+    <section>
+      <h1 className="title">Unsplash Images</h1>
+      <form onSubmit={handleSubmit} className="search-form">
+        <input
+          type="text"
+          className="form-input search-input"
+          placeholder="dog"
+          name="search"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
